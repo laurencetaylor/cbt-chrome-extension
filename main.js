a3c7e4bbaa8b14fe4eb749dd62b9b2a76f4cf8a1
Laurence Taylor <ltaylorapp@gmail.com>
2021-03-05 21:41:50 +0000

    Update distortion schema

--- main.js
+++ main.js
@@ -1,19 +1,75 @@
 const distortionDetails = {
-    ALL_OR_NOTHING: { name: 'All Or Nothing Thinking', description: '' },
-    OVERGENERALIZATION: { name: 'Overgeneralization', description: '' },
-    MENTAL_FILTER: { name: 'Mental Filter', description: '' },
+    ALL_OR_NOTHING: {
+        name: 'All Or Nothing Thinking',
+        description:
+            'Evaluating things in extreme, black or white categories; dichotomous thinking',
+        example:
+            '',
+    },
+    OVERGENERALIZATION: {
+        name: 'Overgeneralization',
+        description:
+            'If something happens once, or coincidentally multiple times, it will happen forever',
+        example: 'You are rejected in a job interview; you conclude you will never find work again',
+    },
+    MENTAL_FILTER: {
+        name: 'Mental Filter',
+        description:
+            'Taking one small negative event and dwelling on it. An opposite overgeneralization: instead of taking a small event and overgeneralizing, you focus on it exclusively and filter anything else out',
+        example: '',
+    },
     DISQUALIFY_THE_POSITIVE: {
         name: 'Disqualify the positive',
-        description: '',
+        description:
+            'Disqualifying neutral or positive experiences, so they are transformed into negative ones',
+        example: '',
     },
-    MIND_READING: { name: 'Mind Reading', description: '' },
-    FORTUNE_TELLING: { name: 'Fortune Telling', description: '' },
-    MAGNIFICATION: { name: 'Magnification', description: '' },
-    MINIMIZATION: { name: 'Minimization', description: '' },
-    EMOTIONAL_REASONING: { name: 'Emotional Reasoning', description: '' },
-    SHOULD_STATEMENTS: { name: 'Should Statements', description: '' },
-    MISLABELLING: { name: 'Mislabelling', description: '' },
-    PERSONALIZATION: { name: 'Personalization', description: '' },
+    MIND_READING: {
+        name: 'Mind Reading',
+        description:
+            'Invalidating neutral or positive experiences so they are transformed into negative ones',
+        example: '',
+    },
+    FORTUNE_TELLING: {
+        name: 'Fortune Telling',
+        description: 'Imagining something bad is about to happen',
+        example: '',
+    },
+    MAGNIFICATION: {
+        name: 'Magnification',
+        description:
+            'Exaggerating your errors, fears, and imperfections; catastrophising',
+        example: '',
+    },
+    MINIMIZATION: {
+        name: 'Minimization',
+        description:
+            'Glossing over your desirable qualities as well as the flaws of others',
+        example: '',
+    },
+    EMOTIONAL_REASONING: {
+        name: 'Emotional Reasoning',
+        description: 'Taking emotions as evidence for the truth',
+        example: '',
+    },
+    SHOULD_STATEMENTS: {
+        name: 'Should Statements',
+        description:
+            "Making yourself feel terrible by saying 'I should' do this or that",
+        example: '',
+    },
+    MISLABELLING: {
+        name: 'Mislabelling',
+        description:
+            'Describing events with words that are inaccurate and emotionally heavily loaded',
+        example: '',
+    },
+    PERSONALIZATION: {
+        name: 'Personalization',
+        description:
+            'Assuming responsibility for negative events where there is no basis for doing so',
+        example: '',
+    },
 };
 
 const getRandomDistortion = () => {
