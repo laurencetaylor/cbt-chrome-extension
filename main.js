aa4901a1dd95d7430e82a66eb61cd2df493ce98d
Laurence Taylor <30178403+laurencetaylor@users.noreply.github.com>
2023-05-20 21:49:18 +0100

    Make distortion list tabbable

--- main.js
+++ main.js
@@ -24,53 +24,64 @@ const distortionDetails = {
         name: 'Disqualify The Positive',
         description:
             'Invalidating neutral or positive experiences so they are transformed into negative ones. For example, responding to a compliment with "they\'re just being nice".',
-        example: "Try to avoid throwing cold water on good things when they happen. Embrace them and their richness.",
+        example:
+            'Try to avoid throwing cold water on good things when they happen. Embrace them and their richness.',
     },
     MIND_READING: {
         name: 'Mind Reading',
         description:
             'Assuming arbitrarily that other people are looking down on you.',
-        example: 'People have a lot going on in their lives. Behaviors you perceive as an affront are likely unrelated to your actions or personality.',
+        example:
+            'People have a lot going on in their lives. Behaviors you perceive as an affront are likely unrelated to your actions or personality.',
     },
     FORTUNE_TELLING: {
         name: 'Fortune Telling',
-        description: 'Imagining arbitrarily that something bad is about to happen. Making negative predictions with no evidence behind them.',
-        example: 'Acknowledge the silliness of your predictions. Try to avoid baselessly imagining bad things happening.',
+        description:
+            'Imagining arbitrarily that something bad is about to happen. Making negative predictions with no evidence behind them.',
+        example:
+            'Acknowledge the silliness of your predictions. Try to avoid baselessly imagining bad things happening.',
     },
     MAGNIFICATION: {
         name: 'Magnification',
         description:
             'Exaggerating your errors, fears, and imperfections; catastrophizing and blowing things out of proportion.',
-        example: 'Making a mistake does not mean your life will spiral out of control. Have empathy for yourself.',
+        example:
+            'Making a mistake does not mean your life will spiral out of control. Have empathy for yourself.',
     },
     MINIMIZATION: {
         name: 'Minimization',
         description:
             'Minimizing your desirable qualities and strengths, as well as the flaws of others.',
-        example: 'Be proud of your many good qualities. Accept that nobody is perfect.',
+        example:
+            'Be proud of your many good qualities. Accept that nobody is perfect.',
     },
     EMOTIONAL_REASONING: {
         name: 'Emotional Reasoning',
-        description: 'Taking emotions as evidence for the truth. You feel like a failure, so you must be a failure.',
-        example: "Remember that negative emotions are not always rooted in reality, they are often the result of distorted perceptions.",
+        description:
+            'Taking emotions as evidence for the truth. You feel like a failure, so you must be a failure.',
+        example:
+            'Remember that negative emotions are not always rooted in reality, they are often the result of distorted perceptions.',
     },
     SHOULD_STATEMENTS: {
         name: 'Should Statements',
         description:
             "Making yourself feel terrible by saying 'I should' or 'I must' do this or that. Believing that other people 'should' behave in certain ways.",
-        example: "Try replacing 'should' with 'it would be nice if'. Alternatively, your 'should' might not even be accurate or rooted in reality.",
+        example:
+            "Try replacing 'should' with 'it would be nice if'. Alternatively, your 'should' might not even be accurate or rooted in reality.",
     },
     MISLABELLING: {
         name: 'Mislabelling',
         description:
             'Describing events with words that are inaccurate and emotionally heavily loaded. For example, labelling yourself as a failure after one mistake.',
-        example: 'Remember the negative labels are overly-simplistic and wrong. Your entire complex self can never be associated with just one label.',
+        example:
+            'Remember the negative labels are overly-simplistic and wrong. Your entire complex self can never be associated with just one label.',
     },
     PERSONALIZATION: {
         name: 'Personalization',
         description:
             'Arbitrarily assuming responsibility for negative events when there is no basis for doing so.',
-        example: 'Just because something bad happens does not mean it is your fault.',
+        example:
+            'Just because something bad happens does not mean it is your fault.',
     },
 };
 
@@ -82,8 +93,7 @@ const getRandomDistortionKey = () => {
     return randomDistortionKey;
 };
 
-// Controller
-const fillDistortionDetails = (distortionKey) => {
+const renderDistortionDetails = (distortionKey) => {
     const { name, description, example } = distortionDetails[distortionKey];
 
     const distortionNameElement = document.querySelector('.distortion-name');
@@ -99,53 +109,83 @@ const fillDistortionDetails = (distortionKey) => {
     distortionExampleElement.innerHTML = example;
 };
 
-const highlightDistortion = (distortionKey) => {
+const highlightDistortionName = (distortionKey) => {
     const selectedDistortion = document.querySelector(`.${distortionKey}`);
     selectedDistortion.style.color = '#cb9d06';
 };
 
-const unhighlightDistortion = (distortionKey) => {
+const unhighlightDistortionName = (distortionKey) => {
     const distortion = document.querySelector(`.${distortionKey}`);
     distortion.style.color = 'black';
 };
 
-const setLeafSvg = () => {
+const renderRandomLeafSvg = () => {
+    const NUMBER_OF_SVGS = 5;
+
     const svgPlaceholder = document.querySelector('.leaf-image');
-    const randomNumber = Math.floor(Math.random() * 5);
+
+    const randomNumber = Math.floor(Math.random() * NUMBER_OF_SVGS);
+
     svgPlaceholder.setAttribute('data', `plant-${randomNumber}.svg`);
 };
 
-let currentDistortionKey = getRandomDistortionKey();
+let currentDistortionKey;
 
-const fillDistortionList = () => {
-    const distortionUlElement = document.querySelector('.distortion-list');
+const changeSelectedDistortion = (newDistortionKey) => {
+    unhighlightDistortionName(currentDistortionKey);
 
-    Object.keys(distortionDetails).forEach((distortionKey) => {
-        const liElement = document.createElement('li');
-        liElement.setAttribute('class', distortionKey);
+    renderDistortionDetails(newDistortionKey);
+    highlightDistortionName(newDistortionKey);
 
-        const distortionName = distortionDetails[distortionKey].name;
+    currentDistortionKey = newDistortionKey;
+};
 
-        liElement.innerHTML = distortionName;
+const renderDistortionListElement = (
+    distortionKey,
+    distortionListContainer,
+    index
+) => {
+    const liElement = document.createElement('li');
 
-        liElement.addEventListener('click', (event) => {
-            unhighlightDistortion(currentDistortionKey);
+    liElement.setAttribute('class', distortionKey);
+    liElement.setAttribute('tabindex', index + 1);
 
-            currentDistortionKey = event.target.className;
+    const distortionName = distortionDetails[distortionKey].name;
 
-            fillDistortionDetails(currentDistortionKey);
-            highlightDistortion(currentDistortionKey);
-        });
+    liElement.innerHTML = distortionName;
 
-        distortionUlElement.appendChild(liElement);
+    liElement.addEventListener('click', (event) => {
+        changeSelectedDistortion(event.target.className);
+    });
+
+    liElement.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
+            changeSelectedDistortion(event.target.className);
+        }
+    });
+
+    distortionListContainer.appendChild(liElement);
+};
+
+const renderDistortionList = () => {
+    const distortionListContainer = document.querySelector('.distortion-list');
+
+    Object.keys(distortionDetails).forEach((distortionKey, index) => {
+        renderDistortionListElement(
+            distortionKey,
+            distortionListContainer,
+            index
+        );
     });
 };
 
 window.onload = () => {
-    fillDistortionList();
+    renderDistortionList();
 
-    fillDistortionDetails(currentDistortionKey);
-    highlightDistortion(currentDistortionKey);
+    currentDistortionKey = getRandomDistortionKey();
 
-    setLeafSvg();
+    renderDistortionDetails(currentDistortionKey);
+    highlightDistortionName(currentDistortionKey);
+
+    renderRandomLeafSvg();
 };
